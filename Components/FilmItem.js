a76814a0a4223ab792fc7531e806d18d4cff7623
Thomas Milton <perso.milton@gmail.com>
2022-03-17 08:42:53 +0100

    ajout de template JSon Adventure_Time

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
+import 'react-json-pretty/themes/adventure_time.css'
+import JSONPretty from 'react-json-pretty'
 
 class FilmItem extends React.Component {
   render() {
@@ -7,10 +9,22 @@ class FilmItem extends React.Component {
     console.log(this.props.film.title)
     return (
       <View>
+        <JSONPretty data={film}></JSONPretty>
         <Text>{film.title}</Text>
+        <Image
+          style={styles.image}
+          source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 20,
+    height: 20,
+  },
+})
 
 export default FilmItem

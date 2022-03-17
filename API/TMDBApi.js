b57adebb0d6a775c9df71e47e4ffc6cead472495
Thomas Milton <perso.milton@gmail.com>
2022-03-17 14:44:16 +0100

    ajout de la fonction recherche avec titre au choix

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -3,10 +3,16 @@ import { API_TOKEN } from '@env'
 
 const getFilmsFromApiWithSearchedText = async (text) => {
   const url =
-    'https://api.themoviedb.org/3/movie/550?api_key=' +
+    'https://api.themoviedb.org/3/search/movie?api_key=' +
     API_TOKEN +
     '&language=fr&query=' +
     text
+  const response = await axios.get(url)
+  console.log('--getFilmsFromApiWithSearchedText--')
+  console.log(url)
+  console.log(response.data)
+  console.log('--fin getFilmsFromApiWithSearchedText--')
+  return response.data
 }
 
 export default getFilmsFromApiWithSearchedText
--- Components/Search.js
+++ Components/Search.js
@@ -1,8 +1,9 @@
 // Components/Search.js
 import React from 'react'
 import { FlatList, View, TextInput, Button, StyleSheet } from 'react-native'
-import films from '../Helpers/filmsData'
+// import films from '../Helpers/filmsData'
 import FilmItem from '../Components/FilmItem'
+import getFilmsFromApiWithSearchedText from '../API/TMDBApi'
 
 const styles = StyleSheet.create({
   main_container: {
@@ -30,17 +31,37 @@ const styles = StyleSheet.create({
 
 class Search extends React.Component {
   render() {
+    console.log('RENDER')
     return (
       <View style={styles.main_container}>
-        <TextInput placeholder="Titre du film" style={styles.textinput} />
-        <Button title="Rechercher" onPress={() => {}} />
+        <TextInput
+          placeholder="Titre du film"
+          style={{ styles }}
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />{' '}
+        <Button title="Rechercher" onPress={() => this._loadFilms()} />
         <FlatList
-          data={films}
+          data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => <FilmItem film={item} />}
         />
       </View>
     )
+  }
+  constructor(props) {
+    super(props)
+    this.searchedText = ''
+    this.state = { films: [] }
+  }
+  
+  _searchTextInputChanged(text) {
+    this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par setState
+  }
+
+  _loadFilms() {
+    getFilmsFromApiWithSearchedText(this.searchedText).then((data) => {
+      this.setState({ films: data.results })
+    })
   }
 }
 
